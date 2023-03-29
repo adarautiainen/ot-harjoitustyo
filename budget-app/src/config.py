3d6dbade64cad0f0fac4a4651ca7607885a32c92
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-03-29 13:49:17 +0300

    database connection edit

--- budget-app/src/config.py
+++ budget-app/src/config.py
@@ -1,7 +1,13 @@
 import os
 from dotenv import load_dotenv
 
+path = "db/db.sqlite3"
+
 dirname = os.path.dirname(__file__)
+
+dbpath = os.path.join(dirname, path)
+
+os.makedirs(os.path.dirname(dbpath), exist_ok=True)
 
 try:
     load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
