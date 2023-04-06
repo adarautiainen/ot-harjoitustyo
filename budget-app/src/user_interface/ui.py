80cca4cd9f3f2502b52efee05dbd6c15893ab3af
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-04-06 13:09:02 +0300

    modified ui

--- budget-app/src/user_interface/ui.py
+++ budget-app/src/user_interface/ui.py
@@ -8,33 +8,34 @@ class UI:
         self._current = None
 
     def start(self):
-        self._login_view()
-
-    def _login_view(self):
-        self._hide_current()
-
-        self._current = Login(
-            self._root,
-            self._create_user_view
-        )
-
-        self._current.pack()
-
-    def _create_user_view(self):
-        self._hide_current()
-
-        self._current = CreateUser(
-            self._root,
-            self._login_view
-        )
-
-        self._current.pack()
-
+        self._login_show()
 
     def _hide_current(self):
         if self._current:
             self._current.destroy()
 
         self._current = None
+
+    def _login_show(self):
+        self._hide_current()
+
+        self._current = Login(
+            self._root,
+            self._create_user_show()
+        )
+
+        self._current.pack()
+
+    def _create_user_show(self):
+        self._hide_current()
+
+        self._current = CreateUser(
+            self._root,
+            self._login_show()
+        )
+
+        self._current.pack()
+
+
 
 
