25ec8312877c2c81f64b5bb7566d77139091f6f0
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-04-12 09:39:15 +0300

    modified

--- budget-app/tasks.py
+++ budget-app/tasks.py
@@ -10,7 +10,7 @@ def test(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branc -m pytest src", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 
 @task(coverage)
