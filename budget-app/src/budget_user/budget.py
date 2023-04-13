5915cab14a8d961dedf046caf8598c0cbadc0b80
Ada Rautiainen <ada.rautiainen@gmail.com>
2023-04-13 10:38:02 +0300

    modified

--- budget-app/src/budget_user/budget.py
+++ budget-app/src/budget_user/budget.py
@@ -2,15 +2,8 @@ import uuid
 
 
 class Budget:
-    # This class describes individual budget
 
     def __init__(self, content, value, user=None, budget_id=None):
-
-        # content: string
-        # value: int
-        # user: describes owner of budget
-        # budget_id: describes budgets' id
-
         self.content = content
         self.value = value
         self.user = user
--- budget-app/src/config.py
+++ budget-app/src/config.py
@@ -1,20 +1,8 @@
 import os
-from dotenv import load_dotenv
-
-path = "db/db.sqlite3"  # pylint: disable=invalid-name
 
 dirname = os.path.dirname(__file__)
 
-dbpath = os.path.join(dirname, path)
-
-os.makedirs(os.path.dirname(dbpath), exist_ok=True)
-
-try:
-    load_dotenv(dotenv_path=os.path.join(dirname, "..", ".env"))
-except FileNotFoundError:
-    pass
-
-BUDGET_FILENAME = os.getenv("BUDGET_FILENAME") or "budgets.csv"
+BUDGET_FILENAME = os.getenv("BUDGET_FILENAME") or "budgets.sqlite"
 BUDGET_FILE_PATH = os.path.join(dirname, "..", "data", BUDGET_FILENAME)
 
 DATABASE_FILENAME = os.getenv("DATABASE_FILENAME") or "database.sqlite"
